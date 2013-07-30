4b7105e14a5773c0cca040b7a703c50b1a662a83
Juan Pablo Rodriguez <nemesishidden@gmail.com>
2013-07-30 10:27:19 -0400

    Update db_manager.js

--- controller/db_manager.js
+++ controller/db_manager.js
@@ -125,7 +125,7 @@ var baseDatos = {
                 document.getElementById('presupuestoValidoHasta').innerHTML = r.fechaValido
                 document.getElementById('presupuestoDisponible').innerHTML = r.disponible
                 document.getElementById('presupuestoUtilizado').innerHTML = r.utilizado;
-
+                
                 document.getElementById('presupuestoValidoHastaDos').innerHTML = r.fechaValido
                 document.getElementById('presupuestoDisponibleDos').innerHTML = r.disponible
                 document.getElementById('presupuestoUtilizadoDos').innerHTML = r.utilizado;
